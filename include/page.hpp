c7e2bfa8529c452ee06c9ed8a0065909b77b3cdf
daniilgriga <daniilextra13@gmail.com>
2025-09-26 22:30:20 +0300

    added operator << for Page_t

--- include/page.hpp
+++ include/page.hpp
@@ -10,7 +10,12 @@ struct Page_t
     Page_t (int id, const std::string& data) : page_id_(id), data_(data) {}
 };
 
+inline std::ostream& operator<< (std::ostream& ostream, const Page_t& page)
+{
+    return ostream << "[Page " << page.page_id_ << ": " << page.data_ << "]";
+}
+
 static Page_t slow_get_page (const int& key)
 {
-    return {key, "Page number " + std::to_string(key) + ": some data :)"};
+    return {key, "Page number " + std::to_string(key) + " with some data :)"};
 }
