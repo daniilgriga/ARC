dc965e63060ad644dab4034a6aea0ac65127cb5c
daniilgriga <daniilextra13@gmail.com>
2025-09-27 00:13:05 +0300

    added lookup_update func for IdealCache_t

--- include/ideal_cache.hpp
+++ include/ideal_cache.hpp
@@ -3,12 +3,13 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-#include <cstdef>
+#include <cstddef>
 
 template <typename T, typename KeyT = int>
-class IdealCache
+class IdealCache_t
 {
     size_t size_;
+    size_t curr_position_;
 
     using PositionMap = std::unordered_map<KeyT, std::vector<int>>;
     using CacheStore  = std::unordered_map<KeyT, T>;
@@ -16,50 +17,63 @@ class IdealCache
     PositionMap keys_positions_;
     CacheStore cache_;
 
-    static constexpr int NO_NEXT = -1;
+    static constexpr int NO_NEXT_ = -1;
 
     int find_next_occurrence (const KeyT& key, int curr_index)
     {
         std::vector<int>& vec = keys_positions_[key];
         auto iter = std::lower_bound (vec.begin(), vec.end(), curr_index + 1);  // binary searchng
 
-        return (iter == vec.end() ? NO_NEXT : *iter);
+        return (iter == vec.end() ? NO_NEXT_ : *iter);
     }
 
 public:
-    IdealCache (size_t size) : size_(size) {}
+    IdealCache_t (size_t size) : size_(size), curr_position_(0) {}
 
     void set_sequence (const std::vector<KeyT>& sequence)
     {
         keys_positions_.clear();
 
-        for (size_t i = 1; i < sequence.size(); i++)
+        for (size_t i = 0; i < sequence.size(); i++)
             keys_positions_[sequence[i]].push_back(i);
     }
 
+    template <typename FuncT>
+    bool lookup_update (const KeyT& key, FuncT get_page)
+    {
+        if (cache_.find (key) != cache_.end())
+            return true;                                                        // HIT
 
+        if (cache_.size() < size_)
+        {
+            cache_.try_emplace (key, get_page (key));
+            return false;                                                       // MISS
+        }
+
+        KeyT key_for_evict = 0;
+        int latest_position = NO_NEXT_;
+
+        for (auto iter = cache_.begin(); iter != cache_.end(); iter++)
+        {
+            const KeyT& key_in_cache = iter->first;
+
+            int next_position = find_next_occurrence (key, curr_position_);
+            if (next_position == NO_NEXT_)
+            {
+                key_for_evict = key_in_cache;
+                break;
+            }
+
+            if (next_position > latest_position)
+            {
+                latest_position = next_position;
+                key_for_evict = key_in_cache;
+            }
+        }
+
+        cache_.erase (key_for_evict);
+        cache_.try_emplace (key, get_page (key));
+
+        return false;
+    }
 };
-
-
-
-
-
-/*
-
- 1 2 3 4 3 2 1
-
- |1|-|
-
- |2|1|
-
- мы на тройке, скачиваем ------ алгоритм кеша: какое из 1, 2, 3 встретится позже всех - понимаем, что это единица - |3|2|
-
- на четверке, скачиваем ------ алгоритм кеша: она больше никогда не встретится, значит в кеш не кладем - |3|2|
-
- 3 --- hit
-
- 2 --- hit
-
- 1, скачиваем ------ уже на кеш все равно
-
-*/
